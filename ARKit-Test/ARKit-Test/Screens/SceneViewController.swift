bea23ba7a5d1f183ec0f36cd2524e718825b768d
Mikael Melkonyan <melkonyan2311@mail.ru>
2018-02-11 13:31:19 +0200

    Struct changes

---
+++ ARKit-Test/ARKit-Test/Screens/SceneViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SceneViewController.swift
+//  ARKit-Test
+//
+//  Created by miki on 2/11/18.
+//  Copyright © 2018 Mikael Melkonyan. All rights reserved.
+//
+
+import UIKit
+
+class SceneViewController: UIViewController {
+    
+}
